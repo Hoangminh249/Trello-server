09b773dda5cd1260f62b1af4bacaefdc0e4b2e1e
Hoangminh249 <97183505+Hoangminh249@users.noreply.github.com>
2024-03-19 19:13:10 +0700

    structure routing for project

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,1 +1,13 @@
-/** Hoangminh */
+import express from "express";
+import { StatusCodes } from "http-status-codes";
+import { boardRoutes } from "./boardRoutes";
+
+const Router = express();
+
+Router.get("/status", (req, res) => {
+  res.status(StatusCodes.OK).json({ message: "Success" });
+});
+
+Router.use("/boards", boardRoutes);
+
+export const APIs_V1 = Router;
